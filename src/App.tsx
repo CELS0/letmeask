35aa9fa0a3406d2045347eb90b264d965bed56f5
CELS0 <celsojr.300@gmail.com>
2021-10-06 14:10:27 -0300

    wip: add home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Button/>
+      <Home/>
     </div>
 
   );
