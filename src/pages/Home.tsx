a1a2954619c48f7097da9c05b560420cd37e73a9
CELS0 <celsojr.300@gmail.com>
2021-10-10 11:50:08 -0300

    wip: add room-code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,44 +6,64 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home() {
     const { user, signInWithGoogle } = useAuth();
     const history = useHistory();
-
+    const [roomCode, setRoomCode] = useState('')
     async function handleCreateRoon() {
-        if(!user){
-           await signInWithGoogle();
+        if (!user) {
+            await signInWithGoogle();
         }
 
         history.push('/rooms/new')
     }
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <button onClick={handleCreateRoon} className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Digite o código da sala"
-                        />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
-        </div>
-    )
-}
+
+    async function handleJoinRoon(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist');
+            return;
+        }
+        history.push('/rooms/${roomCode}');
+    }
+        return (
+            <div id="page-auth">
+                <aside>
+                    <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                    <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                    <p>Tire as dúvidas da sua audiência em tempo-real</p>
+                </aside>
+                <main>
+                    <div className="main-content">
+                        <img src={logoImg} alt="Letmeask" />
+                        <button onClick={handleCreateRoon} className="create-room">
+                            <img src={googleIconImg} alt="Logo do Google" />
+                            Crie sua sala com o Google
+                        </button>
+                        <div className="separator">ou entre em uma sala</div>
+                        <form onSubmit={handleJoinRoon}>
+                            <input
+                                type="text"
+                                placeholder="Digite o código da sala"
+                                onChange={event => setRoomCode(event.target.value)}
+                                value={roomCode}
+                            />
+                            <Button type="submit">
+                                Entrar na sala
+                            </Button>
+                        </form>
+                    </div>
+                </main>
+            </div>
+        )
+    }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,35 @@
-export function Room(){
-    return(
-        <h1>Sala</h1>
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/roonCode';
+import '../styles/room.scss'
+
+export function Room() {
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content-header">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>
+                        <RoomCode/>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="O que você quer perguntar?"
+                    />
+                    <div className="form-footer">
+                        <span>Para fazer uma pergunta,<button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
